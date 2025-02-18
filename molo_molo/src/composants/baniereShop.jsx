8f788d1374aba32b5567ce96243451cb2c43b5db
Cedric-20033 <cedricken2003@gmail.com>
2025-02-18 17:46:16 +0100

    création de la page ProductContext pour permettre a ce que la liste des produits soit disponible dans toute l'application, création du fichier ErreurFetchProduct pour géré l'affichage de l'erreur lors de la recherche des produits

---
+++ molo_molo/src/composants/baniereShop.jsx
@@ -0,0 +1,11 @@
+export function BaniereShop() {
+
+    return <>
+        <header className="shop-header text-center py-5">
+            <div className="container">
+                <h1>Boutique</h1>
+                <p>concevez l'espace dont vous avez toujours rêvé.</p>
+            </div>
+        </header>
+    </>
+}
--- molo_molo/src/composants/pages/shop.jsx
+++ molo_molo/src/composants/pages/shop.jsx
@@ -1,11 +1,13 @@
 import { ErreurFetchProduits } from "../autres/erreurFetchProduits"
+import { BaniereShop } from "../baniereShop"
 import { useProducts } from "../context/ProductContext"
 import { Footer } from "./footer"
+import './shop.css'
 
-export function Shop(){
+export function Shop() {
 
     const { products, loading, error } = useProducts() //récupération des articles de l'API
-    
+
     if (loading) return <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
         <div className="text-center">
             <div className="spinner-border text-dark" role="status">
@@ -15,7 +17,13 @@ export function Shop(){
         </div>
     </div>
 
-    if(error) return <ErreurFetchProduits error={error} />
+    if (error) return <ErreurFetchProduits error={error} />
 
-    return <Footer />
+    return <>
+        <BaniereShop />
+        <div className="container mt-3">
+
+        </div>
+        <Footer />
+    </>
 }
